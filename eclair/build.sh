a4234726f2dd07d9f800e615b761d32c24dbcf64
Clay M <ClayMolitor@Live.com>
2025-07-01 11:52:38 -0700

    BUILD-TEST

--- eclair/build.sh
+++ eclair/build.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/env bash
 # Build, run, and commit 
+
 git add *
 
 git commit -m "BUILD-TEST" 
-sudo docker build -t "nix-ctf" . --build-arg RT_USER=jeff && \
+sudo docker build -t "ssh-lab" . --build-arg USER=$1 && \
 sudo docker run -it --rm nix-ctf 
 
 
