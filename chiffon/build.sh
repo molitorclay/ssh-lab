8bf76ac0b055aad4f418a5792eaf42b463c0b666
Clay M <ClayMolitor@Live.com>
2025-07-09 12:55:45 -0700

    fixup! BUILD-TEST

--- chiffon/build.sh
+++ chiffon/build.sh
@@ -2,7 +2,7 @@
 # Build, run, and commit 
 
 git add *
-echo "TEST: " "$@"
+echo "TEST: " $@
 git commit -m "fixup! BUILD-TEST"  
 sudo docker build -t "ssh-lab" . --build-arg "$@" && \
 sudo docker run --privileged -it --rm ssh-lab 
