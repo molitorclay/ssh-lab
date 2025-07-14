8413fb01fcfb2b05ba698ce2ec317987ff3d2fe2
Clay M <ClayMolitor@Live.com>
2025-07-13 18:07:45 -0700

    Adding more images

--- chiffon/build.sh
+++ chiffon/build.sh
@@ -3,7 +3,7 @@
 
 git add *
 git commit --fixup -m "BUILD-TEST"  
-docker build -t "ssh-lab" . "$@" && \
-docker run -it --rm ssh-lab 
-
+#docker build -t "ssh-lab" . "$@" && \
+#docker run -it --rm ssh-lab 
+docker compose up
 
