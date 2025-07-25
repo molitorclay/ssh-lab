4f255e09e54863f347d48f8f87967ba63a5cfe9e
Clay M <ClayMolitor@Live.com>
2025-07-25 08:57:29 -0700

    fixup! BUILD-TEST

--- chiffon/build.sh
+++ chiffon/build.sh
@@ -3,12 +3,14 @@
 
 # names of ssh images to build.
 # There must be a directory for each image.
+if [[ $1 == '--git' ]]; then
+  git add *
+  git commit -m "fixup! BUILD-TEST"  
+fi
 
 SSH_IMAGES="ssh_a ssh_b ssh_c ssh_d ssh_e ssh_f"
 PORTS=(7 9 13 37 53 88 106 113 119 135 139 179 199 389 427 465 548 554 587 631 646 873 990 993 995 1110 1433 1720 1723 1755 1900 2049 2121 2717 3000 3128 3306 3389 3986 4899 5000 5009 5051 5060 5101 5190 5357 5432 5631 5666 5800 5900 6646 7070 8000 8443 8888 9100 3276)
 
-git add *
-git commit -m "fixup! BUILD-TEST"  
 
 # --- Generate random port and password ---
 # Write respective values to ssh_d/port and ssh_d/pass
